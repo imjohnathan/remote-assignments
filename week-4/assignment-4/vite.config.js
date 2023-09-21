ce03a8a83ea4ec1dc87c2e6a24d7d8aa1c6d966f
imjohnathan <o5786486@gmail.com>
2023-09-21 19:08:38 +0800

    github action

--- week-4/assignment-4/vite.config.js
+++ week-4/assignment-4/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/week-4/assignment-3/',
+  base: '/week-4/assignment-4/',
   plugins: [react()],
 })
