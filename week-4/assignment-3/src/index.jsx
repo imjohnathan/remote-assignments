59c6f0f1fd4b35a2e6a0ba8221996152485e64a1
imjohnathan <o5786486@gmail.com>
2023-09-23 10:24:51 +0800

    fix: github action

--- week-4/assignment-3/src/index.jsx
+++ week-4/assignment-3/src/index.jsx
@@ -1,6 +1,6 @@
-import Header from "./components/Header";
-import Hero from "./components/SectionHero";
-import MainContent from "./components/SectionContent";
+import Header from "@/components/Header";
+import Hero from "@/components/SectionHero";
+import MainContent from "@/components/SectionContent";
 
 import "./style.css";
 
--- week-4/assignment-3/vite.config.js
+++ week-4/assignment-3/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/remote-assignments/week-4/assignment-3/dist',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '#root': resolve(__dirname)
+    }
+  }
 })
